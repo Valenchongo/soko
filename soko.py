5ca03cbd45946392c92c1ac8d21d40651ab552d1
Valenchongo <107526652+Valenchongo@users.noreply.github.com>
2022-10-08 14:52:08 -0300

    Update soko.py

--- soko.py
+++ soko.py
@@ -1,34 +1,39 @@
-from shutil import move
-import string
+
+PARED = "#"
+CAJA_OBJETIVO = "*"
+VACÍO = " "
 
 def crear_grilla(grilla):
+    '''Crea una grilla a partir de la descripción del estado inicial.'''
     f = len(grilla)
     for i in range(0,f):
         grilla [i] =list(grilla[i])
     return grilla
 
 def dimensiones(grilla):
+    '''Devuelve una tupla con la cantidad de columnas y filas de la grilla.'''
     filas = int(len(grilla))
     columnas = int(len(grilla[0]))
     return columnas,filas
 
-def hay_pared(grilla,c,f):
-   if grilla[f][c] == "#":
-        return True
+def hay_pared(grilla, c, f):
+    '''Devuelve True si hay una pared en la columna y fila (c, f).'''
+    return grilla[f][c] == PARED
    
-def hay_objetivo(grilla,c,f):
-    if grilla[f][c] == "." or grilla[f][c] =="+" or grilla[f][c] =="*":
-        return True
+def hay_objetivo(grilla, c, f):
+    '''Devuelve True si hay un objetivo en la columna y fila (c, f).'''
+    return grilla[f][c] == "." or grilla[f][c] =="+" or grilla[f][c] ==CAJA_OBJETIVO
     
-def hay_jugador(grilla,c,f):
-    if grilla[f][c] =="@" or grilla[f][c] =="+":
-        return True
+def hay_jugador(grilla, c, f):
+    '''Devuelve True si el jugador está en la columna y fila (c, f).'''
+    return grilla[f][c] =="@" or grilla[f][c] =="+"
   
-def hay_caja(grilla,c,f):
-    if grilla[f][c] == "$" or grilla[f][c] =="*":
-        return True
+def hay_caja(grilla, c, f):
+    '''Devuelve True si hay una caja en la columna y fila (c, f).'''
+    return grilla[f][c] == "$" or grilla[f][c] ==CAJA_OBJETIVO
   
 def juego_ganado(grilla):
+    '''Devuelve True si el juego está ganado.'''
     c,f=dimensiones(grilla)
     for i in range(0,f):
         fila = grilla[i]
@@ -37,99 +42,74 @@ def juego_ganado(grilla):
                 return False        
     return True
 
-def Movimiento(grilla ,pos_x, pos_y, direccion, jugador_solo):
+def Movimiento(grilla, pos_x, pos_y, direccion):
+    '''Función que evalúa que hay en la posición en la cual el jugador se quiere mover para controlar todas las posibilidades que hay en el juego.'''
     grilla_2 = crear_grilla(grilla[:])
+    pos_x_1,pos_x_2,pos_y_1,pos_y_2 = pos_x+direccion[1],pos_x+(direccion[1])*2,pos_y+direccion[0],pos_y+(direccion[0])*2
    
-    if direccion[0] == 0 and direccion[1] ==-1:
-           pos_x_1,pos_x_2,pos_y_1,pos_y_2 = pos_x-1,pos_x-2,pos_y,pos_y 
+    if  hay_caja(grilla_2, pos_y_1, pos_x_1) and hay_objetivo(grilla_2, pos_y_1, pos_x_1):
 
-    elif direccion[0] == 0 and direccion[1] ==1: 
-            pos_x_1,pos_x_2,pos_y_1,pos_y_2 = pos_x+1,pos_x+2,pos_y,pos_y   
-
-    elif direccion[0] == -1 and direccion[1] == 0:
-         pos_x_1,pos_x_2,pos_y_1,pos_y_2 = pos_x,pos_x,pos_y-1,pos_y-2   
-    
-    elif direccion[0] == 1 and direccion[1] ==0:
-         pos_x_1,pos_x_2,pos_y_1,pos_y_2 = pos_x,pos_x,pos_y+1,pos_y+2 
-         
-    if hay_pared(grilla_2,pos_y_1,pos_x_1):
-            return grilla_2 
-
-    elif  hay_caja(grilla_2,pos_y_1,pos_x_1) and hay_objetivo(grilla_2,pos_y_1,pos_x_1):
-            
-            if  grilla_2[pos_x_2][pos_y_2] == " ":  
-             if jugador_solo:    
-              grilla_2[pos_x][pos_y] = " " 
-             else:grilla_2[pos_x][pos_y] = "."  
-             grilla_2[pos_x_1][pos_y_1] = "+"   
-             grilla_2[pos_x_2][pos_y_2] = "$"               
-             return grilla_2            
-
-            if  hay_objetivo(grilla_2,pos_y_2,pos_x_2):
-              if jugador_solo:
-               grilla_2[pos_x][pos_y] = " " 
-              else:grilla_2[pos_x][pos_y] = "." 
-              grilla_2[pos_x_1][pos_y_1]  = "+"   
-              grilla_2[pos_x_2][pos_y_2] = "*"            
-              return grilla_2    
-
-            if hay_pared(grilla_2,pos_y_2,pos_x_2):
-                return grilla_2
-            if hay_caja(grilla_2,pos_y_2,pos_x_2):
-                return grilla_2    
-
-    elif hay_caja(grilla_2,pos_y_1,pos_x_1):          
-
-            if hay_objetivo(grilla_2,pos_y_2,pos_x_2) and grilla_2[pos_x_2][pos_y_2] != "*":
-              if jugador_solo:  
-               grilla_2[pos_x][pos_y] = " "
-              else: grilla_2[pos_x][pos_y] = "."   
-              grilla_2[pos_x_1][pos_y_1] = "@" 
-              grilla_2[pos_x_2][pos_y_2] = "*"             
-              return grilla_2
-                                       
-            if grilla_2[pos_x_2][pos_y_2] == " ": 
-               if jugador_solo:  
-                grilla_2[pos_x][pos_y] = " "
-               else: grilla_2[pos_x][pos_y] = "."  
-               grilla_2[pos_x_1][pos_y_1] = "@"  
-               grilla_2[pos_x_2][pos_y_2] = "$"             
-               return grilla_2
-
-            if hay_caja(grilla_2,pos_y_2,pos_x_2):
-               return grilla_2   
-            if hay_pared(grilla_2,pos_y_2,pos_x_2):
-               return grilla_2 
-            if grilla_2[pos_x_2][pos_y_2] == "*": 
-                return grilla_2       
-
-    elif hay_objetivo(grilla_2, pos_y_1, pos_x_1):
-            if jugador_solo:
-             grilla_2[pos_x][pos_y] = " " 
+        if  grilla_2[pos_x_2][pos_y_2] == VACÍO or hay_objetivo(grilla_2, pos_y_2, pos_x_2):
+            if hay_objetivo(grilla_2, pos_y, pos_x)  == False:    
+                grilla_2[pos_x][pos_y] = VACÍO 
             else:grilla_2[pos_x][pos_y] = "."  
-            grilla_2[pos_x_1][pos_y_1] = "+"           
+            grilla_2[pos_x_1][pos_y_1]  = "+" 
+
+        if  grilla_2[pos_x_2][pos_y_2] == VACÍO:    
+            grilla_2[pos_x_2][pos_y_2] = "$"               
+            return grilla_2            
+
+        elif  hay_objetivo(grilla_2, pos_y_2, pos_x_2):  
+            grilla_2[pos_x_2][pos_y_2] = CAJA_OBJETIVO            
+            return grilla_2    
+            
+        else:
+            return grilla_2  
+
+    elif hay_caja(grilla_2, pos_y_1, pos_x_1):          
+        
+        if hay_objetivo(grilla_2, pos_y_2, pos_x_2) and not hay_caja(grilla_2, pos_y_2, pos_x_2) or grilla_2[pos_x_2][pos_y_2] == VACÍO:
+            if hay_objetivo(grilla_2, pos_y, pos_x)  == False:  
+                grilla_2[pos_x][pos_y] = VACÍO
+            else: grilla_2[pos_x][pos_y] = "." 
+            grilla_2[pos_x_1][pos_y_1] = "@" 
+        
+        if hay_objetivo(grilla_2, pos_y_2, pos_x_2) and not hay_caja(grilla_2, pos_y_2, pos_x_2):
+            grilla_2[pos_x_2][pos_y_2] = CAJA_OBJETIVO                                   
             return grilla_2
+                                       
+        elif grilla_2[pos_x_2][pos_y_2] == VACÍO: 
+            grilla_2[pos_x_2][pos_y_2] = "$"                         
+            return grilla_2
+        else :
+            return grilla_2   
+            
+    elif hay_objetivo(grilla_2, pos_y_1, pos_x_1):
+        if hay_objetivo(grilla_2, pos_y, pos_x)  == False:
+            grilla_2[pos_x][pos_y] = VACÍO 
+        else:grilla_2[pos_x][pos_y] = "."  
+        grilla_2[pos_x_1][pos_y_1] = "+"           
+        return grilla_2
                     
-    elif grilla_2[pos_x_1][pos_y_1] == " ":
-           if jugador_solo: 
-            grilla_2[pos_x][pos_y] = " "  
-           else:
+    elif grilla_2[pos_x_1][pos_y_1] == VACÍO:
+        if hay_objetivo(grilla_2, pos_y, pos_x) == False: 
+            grilla_2[pos_x][pos_y] = VACÍO  
+        else:
             grilla_2[pos_x][pos_y] = "." 
-           grilla_2[pos_x_1][pos_y_1] = "@"
-           return grilla_2
+        grilla_2[pos_x_1][pos_y_1] = "@"
+        return grilla_2
+    else:
+        return grilla_2         
     
-def mover(grilla,direccion):
-     c,f=dimensiones(grilla)
-     for i in range(0,f):
-        grilla[i] =list(grilla[i]) 
+def mover(grilla, direccion):
+    '''Mueve el jugador en la dirección indicada.'''
+    c,f=dimensiones(grilla)
+    for i in range(0,f):
         for p in range (0,c):
-            if grilla[i][p] == "@" or grilla[i][p] == "+" :
-                jugador_solo=False
-                if grilla[i][p] == "@":                    
-                    jugador_solo = True                   
-                pos_x=int(i)
-                pos_y=int(p)
+            if grilla[i][p] == "@" or grilla[i][p] == "+" :               
+                pos_x, pos_y = i,p
                 break
-     nueva_grilla = Movimiento(grilla , pos_x , pos_y , direccion , jugador_solo)
-     return nueva_grilla
+    nueva_grilla = Movimiento(grilla , pos_x , pos_y , direccion)
+    return nueva_grilla
      
+
